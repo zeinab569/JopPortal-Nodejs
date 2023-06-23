9557865e91bf898037bf4b55973fdea46adfb21a
zeinab elazab <zeinabelazzab875@gmail.com>
2023-06-24 00:17:05 +0300

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,121 @@
+import express from "express";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
+import rateLimit from "express-rate-limit";
+
+//ip limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+//router object
+const router = express.Router();
+
+//routes
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - name
+ *        - lastName
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: The Auto-generated id of user collection
+ *          example : DHSASDHJDJHVAJDSVJAVSD
+ *        name:
+ *          type: string
+ *          description: User name
+ *        lastName:
+ *          type: string
+ *          description: User Last Name
+ *        email:
+ *          type: string
+ *          description: user email address
+ *        password:
+ *          type: string
+ *          description: user password should be greater then 6 character
+ *        location:
+ *          type: string
+ *          description: user location city or country
+ *      example:
+ *        id: GDHJGD788BJBJ
+ *        name: John
+ *        lastName: Doe
+ *        email: johndoes@gmail.com
+ *        password: test@123
+ *        location: mumbai
+ */
+
+/**
+ *  @swagger
+ *  tags:
+ *    name: Auth
+ *    description: authentication apis
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *    post:
+ *      summary: register new user
+ *      tags: [Auth]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *      responses:
+ *        200:
+ *          description: user created successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        500:
+ *          description: internal serevr error
+ */
+
+// REGISTER || POPST
+router.post("/register", limiter, registerController);
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *  post:
+ *    summary: login page
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: login successfull
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      500:
+ *        description: something went wrong
+ */
+
+// LOGIN || POST
+router.post("/login", limiter, loginController);
+
+//export
+export default router;
---
+++ server.js
@@ -0,0 +1,89 @@
+// API DOcumenATion
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "swagger-jsdoc";
+// packages imports
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
+
+//securty packges
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+// files imports
+import connectDB from "./config/db.js";
+// routes import
+import testRoutes from "./routes/testRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import errroMiddelware from "./middelwares/errroMiddleware.js";
+import jobsRoutes from "./routes/jobsRoute.js";
+import userRoutes from "./routes/userRoutes.js";
+import mongoose from "mongoose";
+
+//Dot ENV config
+dotenv.config();
+
+// mongodb connection
+connectDB();
+
+//Swagger api config
+//swagger api options
+
+mongoose.set('strictQuery', true);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "Node Expressjs Job Portal Application",
+    },
+    servers: [
+      {
+          url: "http://localhost:8080",
+            //url: "https://nodejs-job-portal-app.onrender.com"
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spec = swaggerDoc(options);
+
+//rest object
+const app = express();
+
+
+//middelwares
+app.use(helmet(``));
+app.use(xss());
+app.use(mongoSanitize());
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/job", jobsRoutes);
+
+//homeroute root
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
+
+//validation middelware
+app.use(errroMiddelware);
+
+
+//port
+const PORT = process.env.PORT || 8080;
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`
+      .bgCyan.white
+  );
+});
